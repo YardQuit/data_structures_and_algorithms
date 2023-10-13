003e4245e371ce0ae757af216310b61ee23fe5c6
michael jones <michael.jones.xm0nn@aleeas.com>
2023-10-13 09:32:34 +0200

    chore: rename functions to better descriptive and shorter.

--- array_merge_sort_00/src/main.rs
+++ array_merge_sort_00/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let array_1 = vec!['a', 'h', 'c', 'e'];
     let array_2 = vec!['b', 'g', 'd', 'f'];
-    let array_3 = combind_arrays(array_1, array_2).unwrap();
-    let array_4 = divide_and_conquer_array(array_3);
+    let array_3 = combind(array_1, array_2).unwrap();
+    let array_4 = divide(array_3);
     match array_4 {
         Some(array) => {
             for element in array {
@@ -14,7 +14,7 @@ fn main() {
     }
 }
 
-fn combind_arrays<T: Ord + Clone>(array_left: Vec<T>, array_right: Vec<T>) -> Option<Vec<T>> {
+fn combind<T: Ord + Clone>(array_left: Vec<T>, array_right: Vec<T>) -> Option<Vec<T>> {
     if array_left.is_empty() && array_right.is_empty() {
         None
     } else if !array_left.is_empty() && array_right.is_empty() {
@@ -29,7 +29,7 @@ fn combind_arrays<T: Ord + Clone>(array_left: Vec<T>, array_right: Vec<T>) -> Op
     }
 }
 
-fn divide_and_conquer_array<T: Ord + Clone>(array: Vec<T>) -> Option<Vec<T>> {
+fn divide<T: Ord + Clone>(array: Vec<T>) -> Option<Vec<T>> {
     if array.is_empty() {
         return None;
     }
@@ -43,13 +43,13 @@ fn divide_and_conquer_array<T: Ord + Clone>(array: Vec<T>) -> Option<Vec<T>> {
     let left = &array[..mid];
     let right = &array[mid..];
 
-    let sorted_left = divide_and_conquer_array(left.to_vec());
-    let sorted_right = divide_and_conquer_array(right.to_vec());
+    let sorted_left = divide(left.to_vec());
+    let sorted_right = divide(right.to_vec());
 
-    merge_sort_arrays(sorted_left?, sorted_right?)
+    merge(sorted_left?, sorted_right?)
 }
 
-fn merge_sort_arrays<T: Ord + Clone>(mut left: Vec<T>, mut right: Vec<T>) -> Option<Vec<T>> {
+fn merge<T: Ord + Clone>(mut left: Vec<T>, mut right: Vec<T>) -> Option<Vec<T>> {
     if left.is_empty() && right.is_empty() {
         return None;
     }
@@ -76,76 +76,76 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn divide_and_conqure_array_success() {
+    fn test_divide_success() {
         let array = vec!['a', 'h', 'c', 'e', 'b', 'g', 'd', 'f'];
         assert_eq!(
-            divide_and_conquer_array(array).unwrap(),
+            divide(array).unwrap(),
             ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         )
     }
     #[test]
-    fn merge_sort_arrays_two_empty_arrays_failure() {
+    fn test_merge_two_empty_arrays_failure() {
         let array_1: Vec<char> = Vec::new();
         let array_2: Vec<char> = Vec::new();
-        assert_eq!(merge_sort_arrays(array_1, array_2), None);
+        assert_eq!(merge(array_1, array_2), None);
     }
     #[test]
-    fn merge_sort_arrays_two_sorted_arrays_success() {
+    fn test_merge_two_sorted_arrays_success() {
         let array_1 = vec!['a', 'b', 'c', 'd'];
         let array_2 = vec!['e', 'f', 'g', 'h'];
         assert_eq!(
-            merge_sort_arrays(array_1, array_2).unwrap(),
+            merge(array_1, array_2).unwrap(),
             ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         );
     }
     #[test]
-    fn test_sort_arrays_left_empty_one_sorted_success() {
+    fn test_merge_left_empty_one_sorted_success() {
         let array_1: Vec<char> = Vec::new();
         let array_2 = vec!['e', 'f', 'g', 'h'];
         assert_eq!(
-            combind_arrays(array_1, array_2).unwrap(),
+            combind(array_1, array_2).unwrap(),
             ['e', 'f', 'g', 'h']
         );
     }
     #[test]
-    fn test_sort_arrays_right_empty_one_sorted_success() {
+    fn test_merge_right_empty_one_sorted_success() {
         let array_1 = vec!['a', 'h', 'c', 'e'];
         let array_2: Vec<char> = Vec::new();
         assert_eq!(
-            combind_arrays(array_1, array_2).unwrap(),
+            combind(array_1, array_2).unwrap(),
             ['a', 'h', 'c', 'e']
         );
     }
     #[test]
-    fn test_combind_arrays_empty_faulure() {
+    fn test_combind_empty_faulure() {
         let array_1: Vec<char> = Vec::new();
         let array_2: Vec<char> = Vec::new();
-        assert_eq!(combind_arrays(array_1, array_2), None);
+        assert_eq!(combind(array_1, array_2), None);
     }
     #[test]
-    fn test_combind_arrays_success() {
+    fn test_combind_success() {
         let array_1 = vec!['a', 'h', 'c', 'e'];
         let array_2 = vec!['b', 'g', 'd', 'f'];
         assert_eq!(
-            combind_arrays(array_1, array_2).unwrap(),
+            combind(array_1, array_2).unwrap(),
             ['a', 'h', 'c', 'e', 'b', 'g', 'd', 'f']
         );
     }
     #[test]
-    fn test_combined_arrays_left_empty_success() {
+    fn test_combined_left_empty_success() {
         let array_1: Vec<char> = Vec::new();
         let array_2 = vec!['b', 'g', 'd', 'f'];
         assert_eq!(
-            combind_arrays(array_1, array_2).unwrap(),
+            combind(array_1, array_2).unwrap(),
             ['b', 'g', 'd', 'f']
         );
     }
     #[test]
-    fn test_combind_arrays_right_empty_success() {
+    fn test_combind_right_empty_success() {
         let array_1 = vec!['a', 'h', 'c', 'e'];
         let array_2: Vec<char> = Vec::new();
         assert_eq!(
-            combind_arrays(array_1, array_2).unwrap(),
+            combind(array_1, array_2).unwrap(),
             ['a', 'h', 'c', 'e']
         );
     }
